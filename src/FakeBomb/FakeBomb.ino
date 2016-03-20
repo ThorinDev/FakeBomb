3b77250bfde258c4b3f5b544724eac8d875c6bab
ThorinDev <c.nethercott@icloud.com>
2016-03-20 17:35:11 +0000

    Added some I2C Keypad stuff

--- src/FakeBomb/FakeBomb.ino
+++ src/FakeBomb/FakeBomb.ino
@@ -4,11 +4,29 @@
 */
 
 #include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const byte numRows = 4;
+const byte numCols = 4;
+char keymap[numRows][numCols] =  {
+{'1', '2', '3', 'A'}, 
+{'4', '5', '6', 'B'}, 
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+byte rowPins[numRows] = {9,8,7,6};
+byte colPins[numCols]= {5,4,3,2};
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
 
 void setup() {
-  // Setup goes here!
+  lcd.begin();
+  lcd.backlight();
 }
 
 void loop() {
-  // Loop goes here!
+  
 }
+
